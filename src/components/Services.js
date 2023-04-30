71db5568d0ce03685ec8083eb0ed861597bf3eec
hridoy <iamnirob334@gmail.com>
2023-05-01 01:36:22 +0600

    complete portfolio source code upload

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,7 +1,110 @@
-import React from 'react';
+import React from "react";
+// //icons
+import { BsArrowUpRight } from "react-icons/bs";
+// //motion
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+
+//services data
+
+const services = [
+  {
+    id: 1,
+    name: "UI/UX design",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam minima veritatis veniam sequi cumque eligendi.",
+    link: "Learn More",
+  },
+  {
+    id: 2,
+    name: "Development",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam minima veritatis veniam sequi cumque eligendi.",
+    link: "Learn More",
+  },
+  {
+    id: 3,
+    name: "Digital Marketing",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam minima veritatis veniam sequi cumque eligendi.",
+    link: "Learn More",
+  },
+  {
+    id: 4,
+    name: "Product Branding",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam minima veritatis veniam sequi cumque eligendi.",
+    link: "Learn More",
+  },
+];
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section id="services" className="section">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row">
+          {/* text & img */}
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0"
+          >
+            <h2 className="h2 text-accent mb-6">WHat I Do.</h2>
+            <h3 className="h3 max-w-[455px] mb-16">
+              I'm a freelance front-end developer with over 5 years of
+              experience
+            </h3>
+            <button type="button" className="btn btn-sm">
+              See My Work
+            </button>
+          </motion.div>
+          {/* services  */}
+          <motion.div
+            variants={fadeIn("left", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1"
+          >
+            {/* services list  */}
+            <div>
+              {services.map((service) => {
+                const { id, name, link, description } = service || {};
+                return (
+                  <div
+                    className="border-b border-white/20 h-[146px] mb-[38px] flex "
+                    key={id}
+                  >
+                    <div className="max-w-[476px]">
+                      <h4 className="text-[20px] tracking-wider font-primary font-semibold mb-6">
+                        {name}
+                      </h4>
+                      <p className="font-secondary leading-tight">
+                        {description}
+                      </p>
+                    </div>
+                    <div className="flex flex-col flex-1 items-end">
+                      <a
+                        href="#"
+                        className="btn w-9 h-9 mb-[42px] flex justify-center items-center"
+                      >
+                        <BsArrowUpRight />
+                      </a>
+                      <a href="#" className="text-gradient text-sm">
+                        {link}
+                      </a>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
